ab364de5ddf78af851985d684abd7d95b347075c
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-20 06:13:29 +0900

    feat: add code length validatoin

--- ascii_art/gen.rb
+++ ascii_art/gen.rb
@@ -5,8 +5,14 @@ ascii_art = File.read(ascii_art_file)
 count = ascii_art.count('#')
 
 code = File.read(code_file).split.join
-code += '#' * [0, count - (code.size + 13)].max
 
-wrapped_code = "eval(%w(#{code})*\"\")"
+sub = count - (code.size + 13)
+if sub.negative?
+  puts 'Number of chars of code must be less than ones of ascii-art.'
+  exit 1
+end
 
-puts ascii_art.gsub('#') { wrapped_code.slice!(0, 1) }
+code += '#' * sub
+code = "eval(%w(#{code})*\"\")"
+
+puts ascii_art.gsub('#') { code.slice!(0, 1) }
